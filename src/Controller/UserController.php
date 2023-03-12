501de4a3b89a753dce96f2434f71fc1bbbb1a7a8
Halim Hmairi <hmairihalim@gmail.com>
2023-03-12 21:44:07 +0100

    fixed UI :arrow_backward:

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/admin')]
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
