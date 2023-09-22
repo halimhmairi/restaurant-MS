76bf45c45ab640aa1c83822aa4de6c9362d2e120
Halim Hmairi <hmairihalim@gmail.com>
2023-09-22 19:39:57 +0100

    Fixed delete Route

--- src/Controller/ConfigController.php
+++ src/Controller/ConfigController.php
@@ -6,22 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
 #[Route('/admin/config', name: 'app_config')]
 class ConfigController extends AbstractController
 {
     public function delete(Request $request): Response
     {   
-        $previousRouteName = $request->headers->get('referer'); 
-
-        $url = $request->getRequestUri(); // Get the URL from the request
-
-
-        dd($previousRouteName,$url);
 
         return $this->render('config/delete.html.twig');
     }
     
-    function redirectBack($routeName) : Response {
+    function redirectBack($routeName,Request $request) : Response {
+
+        $previousRouteName = $request->headers->get('referer'); 
+
+        $url = $request->getRequestUri(); // Get the URL from the request
+ 
         return $this->redirectToRoute($routeName);
     }
 }
